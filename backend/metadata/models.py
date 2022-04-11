bf3c113329fc9ba60f1cf81177f29d824ada84d7
amarcel <amarcel.pro@gmail.com>
2022-04-11 18:32:37 +0200

    to lower case

--- backend/metadata/models.py
+++ backend/metadata/models.py
@@ -8,3 +8,7 @@ class Account(models.Model):
     address = models.CharField(max_length=100, unique=True)
     email = models.EmailField()
     authorized = models.ManyToManyField(Sender, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.address = self.address.lower()
+        return super(Account, self).save(*args, **kwargs)
