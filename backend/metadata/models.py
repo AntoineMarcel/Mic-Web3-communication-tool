79fd74b95a3fdacaaff18725ce37e07132f4bfd6
amarcel <amarcel.pro@gmail.com>
2022-04-11 10:09:07 +0200

    emailfield

--- backend/metadata/models.py
+++ backend/metadata/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Sender(models.Model):
     name = models.CharField(max_length=100)
-    sender = models.TextField(unique=True)
+    sender = models.EmailField(unique=True)
 
 class Account(models.Model):
     address = models.CharField(max_length=100, unique=True)
-    email = models.TextField()
+    email = models.EmailField()
     authorized = models.ManyToManyField(Sender, blank=True)
