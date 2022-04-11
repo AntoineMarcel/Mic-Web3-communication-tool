9fbc3681ec626142a64e5c79265ce864c8e1804b
amarcel <amarcel.pro@gmail.com>
2022-04-11 19:28:18 +0200

    change header

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,8 @@ from metadata.models import Account, Sender
 class Redirect_API(APIView):
     authentication_classes = []
     permission_classes = []
-    def get(self, request, sendBy=None, receiver=None):
+    def get(self, request, sendBy=None, receiver:str=None):
+        receiver = receiver.lower()
         try:
             account = Account.objects.get(address=receiver)
             sender = Sender.objects.get(sender=sendBy)
@@ -25,4 +26,5 @@ class Sender_API(APIView):
             sender = Sender.objects.get(sender=sendBy)
         except:
             return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"status": "success", "data": sender.name}, status=status.HTTP_200_OK)
+        from_header = (f"{sender.name} <{sender.sender.split('@')[0]}@mic.xyz>")
+        return Response({"status": "success", "data": from_header}, status=status.HTTP_200_OK)
