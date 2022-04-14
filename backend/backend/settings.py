65069a4561107a3eee642a9c1a889c4f617149e0
amarcel <amarcel.pro@gmail.com>
2022-04-14 13:33:20 +0200

    static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b1b+96!$uhy(6$(#t!=i(^db7qxsvxzp&@7za2$90(uwuou#=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*"
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/templates/script.js
+++ backend/templates/script.js
@@ -1,58 +1,59 @@
-var sender = "support@buildspace.app"
-var receiver = null
-var signer = null
-var isConnected = false
+$(document).ready(function () {
+      var receiver = null
+      var signer = null
+      var isConnected = false
 
-$("#closeModal").click(function () {
-      $("#myModal").hide()
-});
+      $("#micCloseModal").click(function () {
+            $("#micModal").hide()
+      });
 
-$("#demo").click(function () {
-      test();
-});
+      $("#demo").click(function () {
+            micConnectWallet();
+      });
 
-async function test() {
-      if (window.ethereum != null) {
-            try {
-                  await window.ethereum.request({ method: 'eth_requestAccounts' });
-                  const provider = new ethers.providers.Web3Provider(window.ethereum);
+      async function micConnectWallet() {
+            if (window.ethereum != null) {
+                  try {
+                        await window.ethereum.request({ method: 'eth_requestAccounts' });
+                        const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-                  signer = provider.getSigner();
-                  receiver = await signer.getAddress();
-                  const verify = await fetch(`https://amarcel.pythonanywhere.com/status/${sender}/${receiver}`, { method: "GET" })
-                  const status = ((await verify.json())["status"])
-                  if (status == "not authorized") {
-                        authorize()
+                        signer = provider.getSigner();
+                        receiver = await signer.getAddress();
+                        const verify = await fetch(`https://app.joinmic.xyz/status/${sender}/${receiver}`, { method: "GET" })
+                        const status = ((await verify.json())["status"])
+                        if (status == "not authorized") {
+                              authorize()
+                        }
+                        else if (status == "error") {
+                              $("#myModal").show()
+                        }
+                        else {
+                              isConnected = true
+                              micAfterConnected()
+                        }
+                  } catch (error) {
+                        console.log(error)
+                        return false;
                   }
-                  else if (status == "error") {
-                        $("#myModal").show()
-                  }
-                  else {
-                        isConnected = true
-                        isConnected()
-                  }
-            } catch (error) {
-                  console.log(error)
-                  return false;
             }
       }
-}
 
-$("#modalForm").submit(function (event) {
-      authorize($("#emailModal").val());
-      $("#myModal").hide()
-      event.preventDefault();
-});
+      $("#micModalForm").submit(function (event) {
+            authorize($("#micEmailModal").val());
+            $("#micModal").hide()
+            event.preventDefault();
+      });
 
-async function authorize(email) {
-      const sig = await signer.signMessage("Authorize support@buildspace.app to send mails")
-      const manage = email == null ?
-            await fetch(`https://amarcel.pythonanywhere.com/manage/${sender}/${receiver}/${sig}`, { method: "GET" })
-            :
-            await fetch(`https://amarcel.pythonanywhere.com/manage/${sender}/${receiver}/${sig}/${email}`, { method: "GET" })
-      const status = ((await manage.json())["status"])
-      if (status == "success") {
-            isAuthorized = true
-            isConnected()
+      async function authorize(email) {
+            const sig = await signer.signMessage("Authorize support@buildspace.app to send mails")
+            const manage = email == null ?
+                  await fetch(`https://app.joinmic.xyz/manage/${sender}/${receiver}/${sig}`, { method: "GET" })
+                  :
+                  await fetch(`https://app.joinmic.xyz/manage/${sender}/${receiver}/${sig}/${email}`, { method: "GET" })
+            const status = ((await manage.json())["status"])
+            if (status == "success") {
+                  isAuthorized = true
+                  micAfterConnected()
+            }
       }
-}
+});
