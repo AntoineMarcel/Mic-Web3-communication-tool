7051123c5bc9d2f1e2dac3a4c1bc5180e06b55ba
amarcel <amarcel.pro@gmail.com>
2022-04-14 15:04:42 +0200

    Revert "remove statics"
    This reverts commit 144ccf75fca0bb890799ae7b1367fd16117ec7e5.

---
+++ backend/api/static/mic_button.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+      var receiver = null
+      var signer = null
+
+      $("#micCloseModal").click(function () {
+            $("#micModal").hide()
+      });
+
+      $("#micConnect").click(function () {
+            micConnectWallet();
+      });
+
+      async function micConnectWallet() {
+            if (window.ethereum != null) {
+                  try {
+                        await window.ethereum.request({ method: 'eth_requestAccounts' });
+                        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+                        signer = provider.getSigner();
+                        receiver = await signer.getAddress();
+                        const verify = await fetch(`https://app.joinmic.xyz/status/${sender}/${receiver}`, { method: "GET" })
+                        const status = ((await verify.json())["status"])
+                        if (status == "not authorized") {
+                              authorize()
+                        }
+                        else if (status == "error") {
+                              $("#micModal").show()
+                        }
+                        else {
+                              micAfterConnected()
+                        }
+                  } catch (error) {
+                        console.log(error)
+                        return false;
+                  }
+            }
+      }
+
+      $("#micModalForm").submit(function (event) {
+            authorize($("#micEmailModal").val());
+            $("#micModal").hide()
+            event.preventDefault();
+      });
+
+      async function authorize(email) {
+            const sig = await signer.signMessage("Authorize support@buildspace.app to send mails")
+            const manage = email == null ?
+                  await fetch(`https://app.joinmic.xyz/manage/${sender}/${receiver}/${sig}`, { method: "GET" })
+                  :
+                  await fetch(`https://app.joinmic.xyz/manage/${sender}/${receiver}/${sig}/${email}`, { method: "GET" })
+            const status = ((await manage.json())["status"])
+            if (status == "success") {
+                  micAfterConnected()
+            }
+      }
+});
